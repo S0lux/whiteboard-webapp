3f19b880ae1997f58add228c2d05532646ddc22d
solux <loctranphuoc456@protonmail.com>
2024-09-04 09:07:00 +0700

    [Docs] Docs for upload file function

--- src/uploader/uploader.interface.ts
+++ src/uploader/uploader.interface.ts
@@ -1,6 +1,13 @@
 import { ConfigAndUrlOptions, TransformationOptions } from "cloudinary";
 
 export interface UploaderService {
+  /**
+   * Upload the provided file to a third-party CDN and returns
+   * the id of the file in the CDN context.
+   * @param {Express.Multer.File} file
+   * @param {string} folder
+   * @param {string} public_id
+   */
   uploadFile(file: Express.Multer.File, folder?: string, public_id?: string): Promise<string>;
   deleteFile(publicId: string): Promise<void>;
   getFileUrl(
